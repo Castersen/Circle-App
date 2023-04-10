05d58a6a3a0857ba3852d16761635f3007c29a13
Castersen <collegejcastersen@gmail.com>
2023-04-10 11:44:48 -0700

    Initial project

---
+++ Circle/Circle/CircleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircleApp.swift
+//  Circle
+//
+//  Created by Jonathan Castersen on 4/5/23.
+//
+
+import SwiftUI
+
+@main
+struct CircleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            SignUpView()
+        }
+    }
+}
