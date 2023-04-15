a9dc59e5544c1b6f14a55fff495bb00edb0b9afb
Castersen <collegejcastersen@gmail.com>
2023-04-14 17:24:50 -0700

    Add loading view

---
+++ Circle/Circle/Sign-Up-Sign-In/LoadingView.swift
@@ -0,0 +1,21 @@
+//
+//  LoadingView.swift
+//  Circle
+//
+//  Created by Jonathan Castersen on 4/14/23.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+
+    var body: some View {
+        Text("Loading...")
+    }
+}
+
+struct LoadingView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoadingView()
+    }
+}
